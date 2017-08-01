8a54c23dda98ad1b339c6977948d9f1add61e412
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-08-01 16:18:36 -0400

    init commit

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+/**
+ * Created by phillipdelia on 8/1/17.
+ */
+public class Student {
+
+    String firstName;
+    String lastName;
+    Grade grade;
+
+
+    public Student(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
